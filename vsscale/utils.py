8e723676d30288c3b5e85d8160ac86d80dfc3a82
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-07-29 22:39:12 +0200

    Add merge_clip_props util

---
+++ vsscale/utils.py
@@ -0,0 +1,19 @@
+import vapoursynth as vs
+
+
+def merge_clip_props(*clips: vs.VideoNode, main_idx: int = 0) -> vs.VideoNode:
+    if len(clips) == 1:
+        return clips[0]
+
+    def _merge_props(f: list[vs.VideoFrame], n: int) -> vs.VideoFrame:
+        fdst = f[main_idx].copy()
+
+        for i, frame in enumerate(f):
+            if i == main_idx:
+                continue
+
+            fdst.props.update(frame.props)
+
+        return fdst
+
+    return clips[0].std.ModifyFrame(clips, _merge_props)
