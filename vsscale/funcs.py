236a5cf4afa30fa8bc9d17403850b9663461e850
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-10-17 01:02:40 +0200

    Add `UnsharpedFSRCNNX`

--- vsscale/funcs.py
+++ vsscale/funcs.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Any, Literal
+from typing import Any, Callable, Concatenate, Literal
 
 from vsaa import Nnedi3
 from vsexprtools import ExprOp, combine, norm_expr
 from vskernels import Scaler
-from vsrgtools import RepairMode, repair
+from vsrgtools import LimitFilterMode, RepairMode, limit_filter, median_clips, repair, unsharp_masked
 from vstools import EXPR_VARS, ColorRange, CustomIndexError, CustomOverflowError, P, check_ref_clip, scale_8bit, vs
 
 from .helpers import GenericScaler
@@ -15,7 +15,8 @@ from .shaders import FSRCNNXShader, FSRCNNXShaderT
 
 __all__ = [
     'MergeScalers',
-    'MergedFSRCNNX'
+    'MergedFSRCNNX',
+    'UnsharpedFSRCNNX'
 ]
 
 
@@ -123,3 +124,49 @@ class MergedFSRCNNX(GenericScaler):
             merged.std.MaskedMerge(smooth, ringing_mask(smooth))
 
         return merged
+
+
+class UnsharpedFSRCNNX(GenericScaler):
+    def __init__(
+        self,
+        unsharp_func: Callable[Concatenate[vs.VideoNode, P], vs.VideoNode] = unsharp_masked,
+        merge_mode: LimitFilterMode | bool = True,
+        reference: type[Scaler] | Scaler | vs.VideoNode = Nnedi3(0, opencl=None),
+        fsrcnnx_shader: FSRCNNXShaderT = FSRCNNXShader.x56,
+        *args: P.args, **kwargs: P.kwargs
+    ) -> None:
+        self.unsharp_func = unsharp_func
+
+        self.merge_mode = merge_mode
+
+        self.reference = reference
+        self.fsrcnnx_shader = fsrcnnx_shader
+
+        self.args = args
+        self.kwargs = kwargs
+
+    def scale(  # type: ignore
+        self, clip: vs.VideoNode, width: int, height: int, shift: tuple[float, float] = (0, 0),
+        *, ref: vs.VideoNode | None = None, **kwargs: Any
+    ) -> vs.VideoNode:
+        fsrcnnx = self.fsrcnnx_shader.scale(clip, width, height, shift, **kwargs)
+
+        if not ref and isinstance(self.reference, vs.VideoNode):
+            ref = self.reference
+
+        if ref and shift != (0, 0):
+            ref = self.kernel.shift(ref, shift)
+
+        smooth = ref or self.scaler.scale(clip, width, height, shift)
+
+        check_ref_clip(clip, ref)
+
+        smooth_sharp = self.unsharp_func(smooth, *self.args, **self.kwargs)
+
+        if isinstance(self.merge_mode, LimitFilterMode):
+            return limit_filter(smooth, fsrcnnx, smooth_sharp, self.merge_mode)
+
+        if self.merge_mode:
+            return median_clips(smooth, fsrcnnx, smooth_sharp)
+
+        return combine([smooth, fsrcnnx, smooth_sharp], ExprOp.MIN)
