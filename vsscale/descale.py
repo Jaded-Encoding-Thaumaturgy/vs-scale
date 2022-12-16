901a6258684d3a1d1245fd72bcaf320d50459ad4
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-16 14:45:12 +0100

    Update `descale_fields`

--- vsscale/descale.py
+++ vsscale/descale.py
@@ -453,29 +453,28 @@ def mixed_rescale(
 
 
 def descale_fields(
-    clip: vs.VideoNode, tff: bool | FieldBasedT = True,
-    width: int | None = None, height: int = 720,
-    kernel: KernelT = Catrom, src_top: float = 0.0,
+    clip: vs.VideoNode, width: int | None = None, height: int = 720,
+    tff: bool | FieldBasedT = True, kernel: KernelT = Catrom,
+    src_top: float | tuple[float, float] = 0.0,
+    src_left: float | tuple[float, float] = 0.0,
     debug: bool = False, func: FuncExceptT | None = None
 ) -> vs.VideoNode:
     """
     Descale interwoven upscaled fields, also known as a cross conversion.
 
-    This function also sets a frameprop with the kernel that was used.
-
-    The kernel is set using an py:class:`vskernels.Kernel` object.
-    For more information, check the `vskernels documentation <https://vskernels.encode.moe/en/latest/>`_.
-
-    ``src_top`` allows you to to shift the clip prior to descaling.
+    ``src_top``, ``src_left`` allow you to to shift the clip prior to descaling.
     This may be useful, as sometimes clips are shifted before or after the original upscaling.
 
     :param clip:        Clip to process.
-    :param tff:         Top-field-first. `False` sets it to Bottom-Field-First.
     :param width:       Native width. Will be automatically determined if set to `None`.
     :param height:      Native height. Will be divided by two internally.
-    :param kernel:      py:class:`vskernels.Kernel` object used for the descaling.
-                        This can also be the string name of the kernel (Default: py:class:`vskernels.Catrom`).
+    :param tff:         Top-field-first. `False` sets it to Bottom-Field-First.
+    :param kernel:      py:class:`vskernels.Kernel` used for the descaling.
     :param src_top:     Shifts the clip vertically during the descaling.
+                        Can be a tuple, defining the shift per-field.
+    :param src_left:    Shifts the clip horizontally during the descaling.
+                        Can be a tuple, defining the shift per-field.
+    :param debug:       Set a frameprop with the kernel that was used.
 
     :return:            Descaled GRAY clip.
     """
@@ -490,7 +489,24 @@ def descale_fields(
     clip = FieldBased.ensure_presence(clip, tff, func)
 
     y = get_y(clip).std.SeparateFields()
-    descaled = kernel.descale(y, width, height_field, (src_top, 0))
+
+    if isinstance(src_top, tuple):
+        ff_top, sf_top = src_top
+    else:
+        ff_top = sf_top = src_top
+
+    if isinstance(src_left, tuple):
+        ff_left, sf_left = src_left
+    else:
+        ff_left = sf_left = src_left
+
+    if (ff_top, ff_left) == (sf_top, sf_left):
+        descaled = kernel.descale(y, width, height_field, (ff_top, ff_left))
+    else:
+        descaled = core.std.Interleave([
+            kernel.descale(y[::2], width, height_field, (ff_top, ff_left)),
+            kernel.descale(y[1::2], width, height_field, (sf_top, sf_left))
+        ])
 
     weave_y = descaled.std.DoubleWeave()
 
