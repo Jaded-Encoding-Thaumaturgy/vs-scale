035565790cd66e2ef79da3fbde8d723cd67ed36b
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-28 13:43:18 +0100

    Deprecate ssim_downsample

--- vsscale/scale.py
+++ vsscale/scale.py
@@ -9,7 +9,7 @@ from vsexprtools import aka_expr_available, expr_func
 from vskernels import Catrom, Scaler, ScalerT, SetsuCubic
 from vsrgtools import box_blur, gauss_blur
 from vstools import (
-    CustomValueError, Matrix, MatrixT, PlanesT, Transfer, VSFunction, check_ref_clip, check_variable, core, depth,
+    Matrix, MatrixT, PlanesT, Transfer, VSFunction, check_ref_clip, check_variable, core, depth,
     fallback, get_depth, get_w, inject_self, vs
 )
 
@@ -53,32 +53,7 @@ class DPID(GenericScaler):
 
 
 @dataclass
-class SSIM(Scaler):
-    smooth: float | VSFunction = ((3 ** 2 - 1) / 12) ** 0.5
-    curve: Transfer | bool = False
-    sigmoid: bool = False
-    epsilon: float = 1e-6
-    scaler: ScalerT = Catrom
-
-    @inject_self
-    def scale(  # type: ignore[override]
-        self, clip: vs.VideoNode, width: int, height: int, shift: tuple[float, float] = (0, 0), **kwargs: Any
-    ) -> vs.VideoNode:
-        kwargs |= {
-            'smooth': self.smooth, 'scaler': self.scaler, 'curve': self.curve,
-            'sigmoid': self.sigmoid, 'epsilon': self.epsilon
-        } | kwargs | {'shift': shift}
-
-        return ssim_downsample(clip, width, height, **kwargs)
-
-
-def ssim_downsample(
-    clip: vs.VideoNode, width: int | None = None, height: int = 720,
-    smooth: int | float | VSFunction = ((3 ** 2 - 1) / 12) ** 0.5,
-    scaler: ScalerT = Catrom,
-    curve: Transfer | bool = False, sigmoid: bool = False,
-    shift: tuple[float, float] = (0, 0), epsilon: float = 1e-6
-) -> vs.VideoNode:
+class SSIM(GenericScaler):
     """
     SSIM downsampler is an image downscaling technique that aims to optimize
     for the perceptual quality of the downscaled results.
@@ -88,79 +63,99 @@ def ssim_downsample(
     The solution is derived in closed-form, which leads to the simple, efficient implementation.
     The downscaled images retain perceptually important features and details,
     resulting in an accurate and spatio-temporally consistent representation of the high resolution input.
-
-    `Original gist <https://gist.github.com/Ichunjo/16ab1f893588aafcb096c1f35a0cfb15>`_
-
-    :param clip:        Clip to process.
-    :param width:       Output width. If None, autocalculates using height.
-    :param height:      Output height (Default: 720).
-    :param smooth:      Image smoothening method.
-                        If you pass an int, it specifies the "radius" of the internally-used boxfilter,
-                        i.e. the window has a size of (2*smooth+1)x(2*smooth+1).
-                        If you pass a float, it specifies the "sigma" of gauss_blur,
-                        i.e. the standard deviation of gaussian blur.
-                        If you pass a function, it acts as a general smoother.
-                        Default uses a gaussian blur.
-    :param scaler:      Scaler object used for certain scaling operations.
-                        This can also be the string name of the kernel.
-    :param curve:       Perform a gamma conversion prior to scaling and after scaling.
-                        This must be set for `sigmoid` to function.
-                        If True it will auto-determine the curve based on the input props or resolution.
-                        Can be specified with for example `curve=TransferCurve.BT709`.
-    :param sigmoid:     When True, applies a sigmoidal curve after the power-like curve
-                        (or before when converting from linear to gamma-corrected).
-                        This helps reduce the dark halo artefacts found around sharp edges
-                        caused by resizing in linear luminance.
-                        This parameter only works if `gamma=True`.
-    :param shift:       Shift passed to the kernel.
-
-    :return:            Downsampled clip.
     """
-    assert clip.format
 
-    scaler = Scaler.ensure_obj(scaler, ssim_downsample)
+    smooth: float | VSFunction | None = None
+    """
+    Image smoothening method.
+    If you pass an int, it specifies the "radius" of the internally-used boxfilter,
+    i.e. the window has a size of (2*smooth+1)x(2*smooth+1).
+    If you pass a float, it specifies the "sigma" of gauss_blur,
+    i.e. the standard deviation of gaussian blur.
+    If you pass a function, it acts as a general smoother.
+    Default uses a gaussian blur.
+    """
 
-    if isinstance(scaler, SSIM):
-        raise CustomValueError("You can't have SSIM as a scaler for itself!", SSIM)
+    curve: Transfer | bool | None = None
+    """
+    Perform a gamma conversion prior to scaling and after scaling. This must be set for `sigmoid` to function.
+    If True it will auto-determine the curve based on the input props or resolution.
+    Can be specified with for example `curve=TransferCurve.BT709`.
+    """
 
-    if callable(smooth):
-        filter_func = smooth
-    elif isinstance(smooth, int):
-        filter_func = partial(box_blur, radius=smooth)
-    elif isinstance(smooth, float):
-        filter_func = partial(gauss_blur, sigma=smooth)
+    sigmoid: bool | None = None
+    """When True, applies a sigmoidal curve after the power-like curve
+    (or before when converting from linear to gamma-corrected).
+    This helps reduce the dark halo artefacts found around sharp edges
+    caused by resizing in linear luminance.
+    This parameter only works if `gamma=True`.
+    """
 
-    width = fallback(width, get_w(height, clip))
+    epsilon: float | None = None
+    """Variable used for math operations."""
 
-    if curve is True:
-        curve = Transfer.from_matrix(Matrix.from_video(clip))
+    @inject_self
+    def scale(  # type: ignore[override]
+        self, clip: vs.VideoNode, width: int, height: int, shift: tuple[float, float] = (0, 0),
+        smooth: int | float | VSFunction = ((3 ** 2 - 1) / 12) ** 0.5,
+        curve: Transfer | bool = False, sigmoid: bool = False
+    ) -> vs.VideoNode:
+        assert check_variable(clip, self.scale)
 
-    bits, clip = get_depth(clip), depth(clip, 32)
+        smooth = fallback(self.smooth, smooth)
+        curve = fallback(self.curve, curve)
+        sigmoid = fallback(self.sigmoid, sigmoid)
 
-    if curve:
-        clip = gamma2linear(clip, curve, sigmoid=sigmoid, epsilon=epsilon)
+        if callable(smooth):
+            filter_func = smooth
+        elif isinstance(smooth, int):
+            filter_func = partial(box_blur, radius=smooth)
+        elif isinstance(smooth, float):
+            filter_func = partial(gauss_blur, sigma=smooth)
 
-    l1 = scaler.scale(clip, width, height, shift)
+        width = fallback(width, get_w(height, clip))
 
-    l1_sq, c_sq = [expr_func(x, 'x dup *') for x in (l1, clip)]
+        if curve is True:
+            curve = Transfer.from_matrix(Matrix.from_video(clip))
 
-    l2 = scaler.scale(c_sq, width, height, shift)
+        bits, clip = get_depth(clip), depth(clip, 32)
 
-    m, sl_m_square, sh_m_square = [filter_func(x) for x in (l1, l1_sq, l2)]
+        if curve:
+            clip = gamma2linear(clip, curve, sigmoid=sigmoid, epsilon=self.epsilon)
 
-    if aka_expr_available:
-        merge_expr = f'z dup * SQ! x SQ@ - SQD! SQD@ {epsilon} < 0 y SQ@ - SQD@ / sqrt ?'
-    else:
-        merge_expr = f'x z dup * - {epsilon} < 0 y z dup * - x z dup * - / sqrt ?'
+        l1 = self._scaler.scale(clip, width, height, shift)
 
-    r = expr_func([sl_m_square, sh_m_square, m], merge_expr)
-    t = expr_func([r, m], 'x y *')
-    d = expr_func([filter_func(m), filter_func(r), l1, filter_func(t)], 'x y z * + a -')
+        l1_sq, c_sq = [expr_func(x, 'x dup *') for x in (l1, clip)]
 
-    if curve:
-        d = linear2gamma(d, curve, sigmoid=sigmoid)
+        l2 = self._scaler.scale(c_sq, width, height, shift)
 
-    return depth(d, bits)
+        m, sl_m_square, sh_m_square = [filter_func(x) for x in (l1, l1_sq, l2)]
+
+        if aka_expr_available:
+            merge_expr = f'z dup * SQ! x SQ@ - SQD! SQD@ {self.epsilon} < 0 y SQ@ - SQD@ / sqrt ?'
+        else:
+            merge_expr = f'x z dup * - {self.epsilon} < 0 y z dup * - x z dup * - / sqrt ?'
+
+        r = expr_func([sl_m_square, sh_m_square, m], merge_expr)
+        t = expr_func([r, m], 'x y *')
+        d = expr_func([filter_func(m), filter_func(r), l1, filter_func(t)], 'x y z * + a -')
+
+        if curve:
+            d = linear2gamma(d, curve, sigmoid=sigmoid)
+
+        return depth(d, bits)
+
+
+def ssim_downsample(
+    clip: vs.VideoNode, width: int | None = None, height: int = 720,
+    smooth: int | float | VSFunction = ((3 ** 2 - 1) / 12) ** 0.5,
+    scaler: ScalerT = Catrom,
+    curve: Transfer | bool = False, sigmoid: bool = False,
+    shift: tuple[float, float] = (0, 0), epsilon: float = 1e-6
+) -> vs.VideoNode:
+    import warnings
+    warnings.warn("ssim_downsample is deprecated! You should use SSIM directly!", DeprecationWarning)
+    return SSIM(epsilon=epsilon, scaler=scaler).scale(clip, width, height, shift, smooth, curve, sigmoid)
 
 
 @dataclass
